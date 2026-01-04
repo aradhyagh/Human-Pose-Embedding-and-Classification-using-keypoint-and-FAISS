d26c07aed9eb566b49e0e9d6d86bfb53942bd34f
Aradhya R Phutak <136249270+aradhyagh@users.noreply.github.com>
2026-01-04 16:05:09 +0530

    Update ara_verify.py

--- ara_verify.py
+++ ara_verify.py
@@ -4,3 +4,11 @@ path_prefix = 'pose_index'
 index, labels = load_vector_db(path_prefix)
 print("Number of vectors in index and labels:")
 print(index.ntotal, len(labels))
+
+def value_counts_list(lst):
+    counts = {}
+    for item in lst:
+        counts[item] = counts.get(item, 0) + 1
+    return counts
+
+print('label counts: ', value_counts_list(labels))
